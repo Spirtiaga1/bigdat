a257bb75d055167b4b11c8a0d04fd9cbbae50773
spirtiaga1 <spirtiaga1@yandex.ru>
2019-12-18 00:29:31 +0300

    Решения по Редису и Монго в архиве. По Монго так и не понял как пользоваться функцией поиска.

---
+++ Lesson 11.sql
@@ -0,0 +1,23 @@
+-- Создайте таблицу logs типа Archive. 
+-- Пусть при каждом создании записи в таблицах users, catalogs и products в таблицу 
+-- logs помещается время и дата создания записи, название таблицы, идентификатор первичного ключа и содержимое поля name.
+use example;
+delimiter $$
+CREATE TRIGGER catalogs_log after insert on catalogs 
+for each row
+BEGIN
+insert into logs values(NOW(),'catalogs',NEW.id,NEW.name);
+end $$
+CREATE TRIGGER users_log after insert on users 
+for each row
+BEGIN
+insert into logs values(NOW(),'users',NEW.id,NEW.name);
+end $$
+CREATE TRIGGER products_log after insert on users 
+for each row
+BEGIN
+insert into logs values(NOW(),'products',NEW.id,NEW.name);
+end $$
+drop trigger users_log;
+delimiter ;
+
